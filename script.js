6ce650b82ac9e2089c45bf33fa71712c3b924d14
Jan <jas.gol@poczta.onet.pl>
2024-07-26 13:58:33 +0200

    Add function getHumanChoice
    To get and validate user choice using prompt.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function getComputerChoice(){
     let random, computerChoice;
     random = Math.floor(Math.random()*3);
-    console.log(random);
     if(random == 0) computerChoice = "rock";
     else if(random == 1)    computerChoice = "paper";
     else if(random ==2) computerChoice = "scissors";
     return computerChoice;
+}
+
+function getHumanChoice(){
+    let humanChoice;
+    humanChoice= prompt("Enter your choice:");
+    humanChoice=humanChoice.toLowerCase();
+    if(humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
+        console.error("Invalid choice");
+    }
+    return humanChoice;
 }
